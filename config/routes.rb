b5da933a986baba5cacaf53477734f7dd87b5cd8
Jérémie Horhant <jeremie.horhant@titinux.net>
2008-12-19 19:02:38 +0100

    Création de la partie administration pour les utilisateurs.
    * Création d'un controlleur pour l'administration des utilisateurs.
    * Ajout d'une route pour la partie administration des utilisateurs.
    * Ajustement pour le passage des tests fonctionnels.
    * Suppression des vues et des actions inutiles pour les utilisateurs.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   
   map.namespace :admin do |admin|
     admin.resources :guilds
+    admin.resources :users
   end
   
   map.resources :deposites
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -7,39 +7,8 @@ class UsersControllerTest < ActionController::TestCase
     assert_not_nil assigns(:users)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create user" do
-    assert_difference('User.count') do
-      post :create, :user => { :name => 'New user', :guild => Guild.find_by_name('Famakna Food'), :admin => false}
-    end
-
-    assert_redirected_to user_path(assigns(:user))
-  end
-
   test "should show user" do
     get :show, :id => users(:Bagu).id
     assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => users(:Bagu).id
-    assert_response :success
-  end
-
-  test "should update user" do
-    put :update, :id => users(:Bagu).id, :user => { }
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete :destroy, :id => users(:Bagu).id
-    end
-
-    assert_redirected_to users_path
   end
 end
