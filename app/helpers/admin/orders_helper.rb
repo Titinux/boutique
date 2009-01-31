2b079858635192a177693c1f511596fa991f040c
Jérémie Horhant <jeremie.horhant@titinux.net>
2009-01-31 01:55:15 +0100

    Affichage du status des commandes sous forme de texte.

--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -4,4 +4,12 @@ module Admin::OrdersHelper
       page.insert_html :bottom, 'order_lines', :partial => 'order_line', :locals => { :form_line => OrderLine.new }
     end
   end
+  
+  def orderStatusHash
+    Hash[0, 'Waiting estimate',
+         1, 'Waiting estimate validation',
+         2, 'In preparation',
+         3, 'Waiting delivery',
+         4, 'Achieved']
+  end
 end
