d04f8303b4d77e60b983cdfb83926f3adb65f91a
Jérémie Horhant <jeremie.horhant@titinux.net>
2009-03-09 15:38:59 +0100

    Ajout d'une tâche automatique qui redémarre passenger lors d'un déployement.

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,15 +35,24 @@ namespace :assets do
       run "mkdir -p #{shared_path}/#{name}"
     end
   end
-end  
-  namespace :deploy do
-    desc "Create asset packages for production" 
-    task :after_update_code, :roles => [:web] do
-      run <<-EOF
-        cd #{release_path} && rake asset:packager:build_all
-      EOF
-    end
+end
+
+namespace :deploy do
+  desc "Create asset packages for production" 
+  task :after_update_code, :roles => [:web] do
+    run <<-EOF
+      cd #{release_path} && rake asset:packager:build_all
+    EOF
   end
+end
+
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
 
 after "deploy:update_code" , "deploy:copy_database_configuration"
 after "deploy:update_code" , "assets:symlink"
+after :deploy, "passenger:restart"
