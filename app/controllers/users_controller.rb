4f94e9434c7923016f9e67451952660b6ca6c3c1
Jérémie Horhant <jeremie.horhant@titinux.net>
2009-05-19 16:30:14 +0200

    Les visiteurs peuvent maitenant créer eux même leur compte.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   layout 'public'
 
-  before_filter :authenticated?
+  before_filter :authenticated?, :except => [:new, :create]
+  before_filter :not_autenticated?, :only => [:new, :create]
   
   # GET /user
   # GET /user.xml
@@ -14,9 +15,41 @@ class UsersController < ApplicationController
     end
   end
   
+  # GET /user/new
+  # GET /user/new.xml
+  def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @user }
+    end
+  end
+  
   # GET /user/edit
   def edit
     @user = user_session.user
+  end
+
+  # POST /user
+  # POST /user.xml
+  def create
+    params[:user].delete_if do |key, value|
+      ! %w(name email guild_id password password_confirmation dofusNicknames).include? key
+    end
+    
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        flash[:notice] = t('user.create_success')
+        format.html { redirect_to root_path }
+        format.xml  { render :xml => @user, :status => :created, :location => @user }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
   # PUT /user
@@ -46,6 +79,14 @@ class UsersController < ApplicationController
     unless user_session.login?
       flash[:error] = t('userSession.youHaveToLog')
       redirect_to login_path
+      false
+    end
+  end
+  
+  def not_autenticated?
+    if user_session.login?
+      flash[:error] = t('userSession.youHaveToUnLog')
+      redirect_to root_path
       false
     end
   end
