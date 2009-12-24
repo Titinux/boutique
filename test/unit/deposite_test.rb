758c02108b81013d10e4d296927ca55bcc16cda3
Jérémie Horhant <jeremie.horhant@titinux.net>
2009-12-24 20:16:46 +0100

    Dernière (enfin j'espère) correction sur les dépôts et ajout d'un test unitaire pour le cas de doublon des dépôts.

--- test/unit/deposite_test.rb
+++ test/unit/deposite_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class DepositTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "duplicated deposits shouldn't be saved" do
+    Deposit.delete_all
+    
+    d1 = Deposit.new(:user_id => users(:Toto).id, :asset_id => assets(:Iron).id, :validated => false, :quantity_modifier => 25)
+    assert d1.save
+                    
+    d2 = Deposit.new(:user_id => users(:Toto).id, :asset_id => assets(:Iron).id, :validated => false, :quantity_modifier => 25)   
+    assert !d2.save
   end
 end
