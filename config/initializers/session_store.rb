2e1f704bce33c6d1c958fb1ac966352e1deb6213
Jérémie Horhant <jeremie.horhant@titinux.net>
2010-05-31 19:37:27 +0200

    Mise à jour de la configuration des cookies.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,17 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key    => '_Boutique_session',
-  :secret => '67b0bdc43837fa0a261a666577594b03e1893d3a49fd40eb62b0a6d1c7d48c0e8e99bccf2435880f2dda07fa70c9778e1ea074543cdbcd3fab5bd016f72258dd',
-  :expire_after => 604800
-}
+Rails.application.config.session_store :cookie_store, { :key => '_Boutique_session',
+                                                        :expire_after => 604800
+                                                      }
 
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-#ActionController::Base.session_store =
 Rails.application.config.session_store :active_record_store
