c821538b45e04573e21e381aba9a444c6744cf1b
Jérémie Horhant <jeremie.horhant@titinux.net>
2010-06-10 14:32:26 +0200

    Correction d'une dépréciation dans le fichier d'initialisation de SASS.

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -2,11 +2,11 @@ Sass::Plugin.options[:template_location] = "#{Rails.root}/app/sass"
 Sass::Plugin.options[:cache_location]    = "#{Rails.root}/tmp/sass_cache"
 Sass::Plugin.options[:property_syntax]   = :new
 
-if RAILS_ENV == 'production'
+if Rails.env == 'production'
   Sass::Plugin.options[:style] = :compressed
 end
 
-if RAILS_ENV == 'developpement'
+if Rails.env == 'developpement'
   Sass::Plugin.options[:style] = :expanded
   Sass::Plugin.options[:line_numbers] = true
 end
