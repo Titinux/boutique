c23735e671fae94d841fe5814e48020c5a6a221d
Jérémie Horhant <jeremie.horhant@titinux.net>
2010-06-11 22:49:18 +0200

    En développement le blocage des mails ne provoque plus d'erreur.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,8 +16,6 @@ Boutique::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
   #config.action_mailer.smtp_settings = {
