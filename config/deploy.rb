d2e1f9f12202e5f2fcfe0e1202889bce5cb91124
Jérémie Horhant <jeremie.horhant@titinux.net>
2010-07-20 11:46:28 +0200

    Mise à jour des recettes de déploiement.

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,38 +16,45 @@ role :db,  "192.168.11.2", :primary => true
 
 set :user, 'root'
 set :use_sudo, false
+set :rails_env, 'production'
 
 namespace :deploy do
+  desc "Copy database configration file in the application"
   task :copy_database_configuration do
     production_db_config = "#{deploy_to}/config/production.database.yml"
     run "cp #{production_db_config} #{release_path}/config/database.yml"
   end
 end
 
-namespace :assets do
-  task :symlink, :roles => :app do
-    assets.create_dirs
-    run "rm -rf #{release_path}/public/images && ln -nfs #{shared_path}/images #{release_path}/public/images"
-  end
+namespace :deploy do
+  namespace :assets do
+    desc "Make symlinks"
+    task :symlink, :roles => :app do
+      assets.create_dirs
+      run "rm -rf #{release_path}/public/images && ln -nfs #{shared_path}/images #{release_path}/public/images"
+    end
 
-  task :create_dirs, :roles => :app do
-    %w(index pictures).each do |name|
-      run "mkdir -p #{shared_path}/#{name}"
+    desc "Create directories"
+    task :create_dirs, :roles => :app do
+      %w(index pictures).each do |name|
+        run "mkdir -p #{shared_path}/#{name}"
+      end
+    end
+
+    desc "Create asset packages for production"
+    task :package, :roles => [:web] do
+      run <<-EOF
+        cd #{release_path} && rake asset:packager:build_all
+      EOF
     end
   end
 end
 
 namespace :deploy do
-  desc "Create asset packages for production" 
-  task :after_update_code, :roles => [:web] do
-    run <<-EOF
-      cd #{release_path} && rake asset:packager:build_all
-    EOF
+  desc "Restarting passenger with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
   end
-end
-
-deploy.task :restart, :roles => :app do
-  run "touch #{current_path}/tmp/restart.txt" 
 end
 
 namespace :deploy do
@@ -59,9 +66,9 @@ end
 
 after "deploy:update_code" , "deploy:copy_database_configuration"
 after "deploy:update_code" , "assets:symlink"
+after "deploy:update_code",  "assets:package"
 after "deploy:symlink", "deploy:update_crontab"
 
 after "deploy:stop",    "delayed_job:stop"
 after "deploy:start",   "delayed_job:start"
 after "deploy:restart", "delayed_job:restart"
-
