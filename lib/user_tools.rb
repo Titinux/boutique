1de9460213018ee707a869e106596d8c03616478
Jérémie Horhant <jeremie.horhant@titinux.net>
2010-11-28 19:22:56 +0100

    Suppression de fichiers qui ne sont plus utilisés.

--- lib/user_tools.rb
+++
@@ -1,8 +0,0 @@
-module UserTools
-  def self.passwordReset(user)
-    newPass = Crypt::makeRandomSequence(8)
-    user.password = newPass
-    user.save
-    UsersMailer.user_password_reset(user, newPass).deliver
-  end
-end
