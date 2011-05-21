23fe4303aa14784a1c5e7216727a7bedbddb0850
Jérémie Horhant <jeremie.horhant@titinux.net>
2011-05-21 15:09:44 +0200

    Ajout d'une copie de la licence AGPLv3 au projet. Ajout d'entête sur les controlleurs afin de rappeler la licence du projet.

--- app/controllers/admin/config_tree_controller.rb
+++ app/controllers/admin/config_tree_controller.rb
@@ -1,5 +1,22 @@
+# Boutique is a saleroom website for Dofus resources, originally created
+# for the merchant guild "Les Marchands d'Hyze"
+# Copyright (C) 2011 - Jérémie Horhant (jeremie dot horhant at titinux dot net)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 class Admin::ConfigTreeController < Admin::AdminController
-  
+
   # GET /admin/config_tree
   # GET /admin/config_tree.xml
   def index
@@ -10,18 +27,18 @@ class Admin::ConfigTreeController < Admin::AdminController
       format.xml  { render :xml => @configElements }
     end
   end
-  
+
   # GET /admin/configtree
   def edit
     @configElement = ConfigTree.find(params[:id])
-    
+
     if params[:edit] == 'tree'
       render :action => 'edit_tree'
     else
       render :action => 'edit_value'
     end
   end
-  
+
   # PUT /admin/config_tree/1
   # PUT /admin/config_tree/1.xml
   def update
