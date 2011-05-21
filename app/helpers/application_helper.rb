17a899b943cbe3abcce932090a383eacf1a26b3d
Jérémie Horhant <jeremie.horhant@titinux.net>
2011-05-21 15:16:55 +0200

    Ajout d'entête sur les helpers, les jobs et les models afin de rappeler la licence du projet.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,20 @@
-# Methods added to this helper will be available to all templates in the application.
-#require "#{RAILS_ROOT}/config/menus.rb"
+# Boutique is a saleroom website for Dofus resources, originally created
+# for the merchant guild "Les Marchands d'Hyze"
+# Copyright (C) 2011 - Jérémie Horhant (jeremie dot horhant at titinux dot net)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 module ApplicationHelper
   def block(*args, &block)
     options = args.extract_options!
