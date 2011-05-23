fe6ae1d895ef00c77677ba4ce6757b013cc0f0b4
Jérémie Horhant <jeremie.horhant@titinux.net>
2011-05-23 14:19:27 +0200

    Correction d'un problème avec le cookie de stockage des identifiants de session. La durée du cookie n'étant pas définie celui-ci ne survivait pas au redémarrage du navigateur, cette durée de vie est désormais fixée à 7 jours.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, { :key => '_Boutique_session',
-                                                        :expire_after => 604800
-                                                      }
+#Boutique::Application.config.session_store :cookie_store, key: '_boutique_session', :expire_after => 604800
 
-Rails.application.config.session_store :active_record_store
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+Boutique::Application.config.session_store :active_record_store, :expire_after => 7.days
