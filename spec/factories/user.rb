6c606737988c618853a6e1ed4a2b53db5c1e1587
Jérémie Horhant <jeremie.horhant@titinux.net>
2011-05-30 10:50:46 +0200

    Ajout d'entête sur les specs afin de rappeler la licence du projet.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,28 @@
-Factory.define :user do |f|
-  f.sequence(:name)  {|n| "user#{n}" }
-  f.email  {|a| "#{a.name}@example.com" }
+# Boutique is a saleroom website for Dofus resources, originally created
+# for the merchant guild "Les Marchands d'Hyze"
+# Copyright (C) 2011 - Jérémie Horhant (jeremie dot horhant at titinux dot net)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-  f.password "user_password"
-  f.password_confirmation {|a| a.password }
+Factory.define :user do |u|
+  u.sequence(:name)  {|n| "user#{n}" }
+  u.email  {|a| "#{a.name}@example.com" }
+
+  u.password "user_password"
+  u.password_confirmation {|a| a.password }
+
+  u.confirmed_at { DateTime.now }
 end
 
 Factory.define :gatherer, :parent => :user do |f|
