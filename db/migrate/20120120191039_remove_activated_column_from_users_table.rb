caeaf8480677f5ddae52e710e05da09465b8187e
Jérémie Horhant <jeremie.horhant@titinux.net>
2012-01-20 20:13:29 +0100

    Suppression d'une vielle colonne dans la table des utilisateurs et mise à jour du schéma de BDD.

---
+++ db/migrate/20120120191039_remove_activated_column_from_users_table.rb
@@ -0,0 +1,9 @@
+class RemoveActivatedColumnFromUsersTable < ActiveRecord::Migration
+  def up
+    remove_column :users, :activated
+  end
+
+  def down
+    add_column :users, :activated, :boolean, :default => false, :null => false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,13 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110726113037) do
+ActiveRecord::Schema.define(:version => 20120120191039) do
 
   create_table "administrators", :force => true do |t|
     t.string   "name",               :limit => 25,                  :null => false
     t.string   "email",                             :default => "", :null => false
     t.string   "encrypted_password", :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                     :default => "", :null => false
     t.integer  "sign_in_count",                     :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(:version => 20110726113037) do
     t.string   "last_sign_in_ip"
     t.integer  "failed_attempts",                   :default => 0
     t.datetime "locked_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                        :null => false
+    t.datetime "updated_at",                                        :null => false
   end
 
   add_index "administrators", ["email"], :name => "index_administrators_on_email", :unique => true
@@ -35,31 +35,31 @@ ActiveRecord::Schema.define(:version => 20110726113037) do
     t.string   "name",        :limit => 25, :null => false
     t.integer  "category_id",               :null => false
     t.string   "pictureUri"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   create_table "cart_lines", :force => true do |t|
     t.integer  "cart_id",    :null => false
     t.integer  "asset_id",   :null => false
     t.integer  "quantity",   :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "carts", :force => true do |t|
     t.integer  "user_id",    :null => false
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "categories", :force => true do |t|
     t.string   "name",       :limit => 25, :null => false
     t.string   "pictureUri"
     t.integer  "parent_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
   end
 
   create_table "config_tree", :force => true do |t|
@@ -69,15 +69,15 @@ ActiveRecord::Schema.define(:version => 20110726113037) do
     t.integer  "parent_id"
     t.integer  "lft",        :null => false
     t.integer  "rgt",        :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "config_values", :force => true do |t|
     t.string   "name",           :null => false
     t.integer  "config_tree_id", :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
   create_table "delayed_jobs", :force => true do |t|
@@ -89,16 +89,17 @@ ActiveRecord::Schema.define(:version => 20110726113037) do
     t.datetime "locked_at"
     t.datetime "failed_at"
     t.string   "locked_by"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+    t.string   "queue"
   end
 
   create_table "deposits", :force => true do |t|
     t.integer  "user_id"
     t.integer  "asset_id"
     t.integer  "quantity"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
     t.boolean  "validated",  :default => false, :null => false
   end
 
@@ -107,8 +108,8 @@ ActiveRecord::Schema.define(:version => 20110726113037) do
   create_table "guilds", :force => true do |t|
     t.string   "name"
     t.string   "pictureUri"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "logs", :force => true do |t|
@@ -118,8 +119,8 @@ ActiveRecord::Schema.define(:version => 20110726113037) do
     t.string   "objectType",                :null => false
     t.integer  "objectId",                  :null => false
     t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   create_table "order_lines", :force => true do |t|
@@ -127,15 +128,15 @@ ActiveRecord::Schema.define(:version => 20110726113037) do
     t.integer  "asset_id",                                    :null => false
     t.integer  "quantity",                                    :null => false
     t.decimal  "unitaryPrice", :precision => 10, :scale => 2
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
   end
 
   create_table "orders", :force => true do |t|
     t.integer  "user_id",                                     :null => false
     t.string   "state",      :limit => 30,                    :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
     t.boolean  "dispatched",               :default => false, :null => false
     t.text     "comment"
   end
@@ -143,8 +144,8 @@ ActiveRecord::Schema.define(:version => 20110726113037) do
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
     t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
@@ -156,9 +157,9 @@ ActiveRecord::Schema.define(:version => 20110726113037) do
     t.integer  "guild_id"
     t.string   "password_salt",                       :default => "",    :null => false
     t.string   "encrypted_password",   :limit => 128, :default => "",    :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "email",                :limit => 50,                     :null => false
+    t.datetime "created_at",                                             :null => false
+    t.datetime "updated_at",                                             :null => false
+    t.string   "email",                :limit => 50,  :default => "",    :null => false
     t.boolean  "gatherer",                            :default => false, :null => false
     t.string   "dofusNicknames"
     t.string   "confirmation_token"
