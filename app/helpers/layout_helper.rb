02181100c68a63753ea4092c86274175092af415
Jérémie Horhant <jeremie.horhant@titinux.net>
2013-06-01 22:12:19 +0200

    Restrict flash display to notice, warning and alert messages.

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -19,7 +19,7 @@ module LayoutHelper
   def render_flash_messages
     out = ''
 
-    flash.each do |name, msg|
+    flash.to_hash.slice(:notice, :warning, :alert).each do |name, msg|
       out += content_tag :p, :class => "flash #{name}" do
         flash_message = image_tag "icones/#{name}.png"
         flash_message += content_tag :strong, t("flash.names.#{name}")
