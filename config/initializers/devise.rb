9781d434e88d06825935e8fac944f65dcd10524a
Jérémie Horhant <jeremie.horhant@titinux.net>
2013-06-03 00:01:25 +0200

    Configure mail settings from application.yml.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = 'no-reply@hyze.boutique.bagu.biz'
+  config.mailer_sender = 'no-reply@boutique.hyze.fr'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
