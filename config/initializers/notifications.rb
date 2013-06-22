af0a91e861363c3a40cecdf7500f2f1ceb304a03
Jérémie Horhant <jeremie.horhant@titinux.net>
2013-06-23 00:33:19 +0200

    Use ActiveSupport::Notifications and sidekiq to create an asynchronous event processor.

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -1,3 +1,3 @@
-ActiveSupport::Notifications.subscribe "state_changed.order.dofus_shop" do |name, start, finish, id, payload|
-  Rails.logger.debug "Order ##{payload[:order_id]} receive #{payload[:event]} and switch state from #{payload[:from]} to #{payload[:to]}"
+ActiveSupport::Notifications.subscribe /\.event$/ do |name, start, finish, id, payload|
+  EventWorker.perform_async(name, start, finish, id, payload)
 end
